5b8a2f779a307a16dd6b228c1a647c0c7d7b933a
d.oligov <d.oligov@samsung.com>
2022-03-03 16:49:55 +0200

    Change test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Tabs from './components/Tabs'
 import TabData from './components/TabData'
 import fetchEntities, {fetchUrls, sortEntitiesByName} from './utils'
 import './App.css'
+// import nothing
 
 class App extends Component {
     state = {
